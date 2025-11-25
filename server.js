5122ed54c0c5569daea6e82df632679c280dcde5
umairansari92 <umair.ansari.92@gmail.com>
2025-11-26 01:03:06 +0500

    Fix case-sensitive model filename

--- server.js
+++ server.js
@@ -15,13 +15,13 @@ import namazRoutes from "./routes/namazRoutes.js";
 dotenv.config();
 connectDB();
 const app = express();
-const PORT = process.env.PORT || 5000;
 app.use(
   cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
