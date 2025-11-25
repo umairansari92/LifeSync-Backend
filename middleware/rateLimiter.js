14416f94de0d07a2f3fe1e1bb07b7dbe29b142ec
umairansari92 <umair.ansari.92@gmail.com>
2025-11-25 16:07:14 +0500

    Fix case-sensitive model filename

--- middleware/rateLimiter.js
+++ middleware/rateLimiter.js
@@ -1,18 +1,18 @@
-import rateLimit from "express-rate-limit";
+// import rateLimit from "express-rate-limit";
 
-export const generalLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 10,
-  message: "Too many requests from this IP, please try again after 5 minutes",
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+// export const generalLimiter = rateLimit({
+//   windowMs: 5 * 60 * 1000, // 5 minutes
+//   max: 10,
+//   message: "Too many requests from this IP, please try again after 5 minutes",
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
 
-export const authLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 5,
-  message:
-    "Too many login/registration attempts, please try again in 15 minutes",
-  standardHeaders: true,
-  legacyHeaders: false,
-});
+// export const authLimiter = rateLimit({
+//   windowMs: 15 * 60 * 1000,
+//   max: 5,
+//   message:
+//     "Too many login/registration attempts, please try again in 15 minutes",
+//   standardHeaders: true,
+//   legacyHeaders: false,
+// });
