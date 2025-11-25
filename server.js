625e990c91e852b348f2b6d2de6e8f843b5ca568
umairansari92 <umair.ansari.92@gmail.com>
2025-11-26 00:59:43 +0500

    Fix case-sensitive model filename

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   })
 );
