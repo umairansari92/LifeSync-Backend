ba096678524644319cdc03ec2c7585f7993e78a7
umairansari92 <umair.ansari.92@gmail.com>
2025-12-04 23:39:52 +0500

    Note Section Update

--- models/note.js
+++ models/note.js
@@ -2,13 +2,14 @@ import mongoose from "mongoose";
 
 const noteSchema = new mongoose.Schema(
   {
-    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-    title: { type: String, required: true },
-    content: { type: String, default: "" },
+    title: String,
+    content: String,
+    color: { type: String, default: "#FFFFFF" },
     pinned: { type: Boolean, default: false },
     archived: { type: Boolean, default: false },
     trashed: { type: Boolean, default: false },
-    color: { type: String, default: "#FFFFFF" },
+    tags: [{ type: String }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   },
   { timestamps: true }
 );
