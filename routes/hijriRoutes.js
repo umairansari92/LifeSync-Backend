05bc3c6691c6d89daedadc76fff070b48a76545b
umairansari92 <umair.ansari.92@gmail.com>
2025-12-27 01:15:18 +0500

    Hijri Date intigerated

--- routes/hijriRoutes.js
+++ routes/hijriRoutes.js
@@ -1,9 +1,8 @@
 import express from "express";
-import getHijriDate from "../controllers/hijriController";
+import { getHijriDate } from "../controllers/hijriController.js";
 
 const router = express.Router();
 
-// /api/hijri?lat=..&lng=..&adjustment=..
 router.get("/hijri", getHijriDate);
 
 export default router;
