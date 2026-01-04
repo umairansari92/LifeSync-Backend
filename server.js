4e8a57666f03397fb51e3358bfc6baaf12ac581a
umairansari92 <umair.ansari.92@gmail.com>
2026-01-04 23:57:54 +0500

    Loan Productivity Added

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.set("trust proxy", 1);
 const allowedOrigins = [
   process.env.CLIENT_URL,
   "http://localhost:5173",
-  "https://lifesynchub.vercel.app/",
+  "https://lifesynchub.vercel.app",
 ];
 
 const corsOptions = {
