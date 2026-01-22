b77cb8dd8ce6387f71973efb88ce3cbae1107a7f
umairansari92 <umair.ansari.92@gmail.com>
2026-01-23 01:26:59 +0500

    Revert "Fix: Use getMonthlyIncome for filtered API responses"
    This reverts commit 40cb329f92ba37068492534ded61228c097a4885.

--- routes/incomeRoutes.js
+++ routes/incomeRoutes.js
@@ -4,24 +4,15 @@ import {
   addIncome,
   deleteIncome,
   getIncomes,
-  getMonthlyIncome,
   updateIncome,
+  
 } from "../controllers/incomeController.js";
 import { generalLimiter } from "../middleware/rateLimiter.js";
 
 const router = express.Router();
 
 router.post("/", protect, generalLimiter, addIncome);
-
-// Smart GET route - uses getMonthlyIncome if query params exist, otherwise getIncomes
-router.get("/", protect, generalLimiter, (req, res, next) => {
-  // Check if month and year query params exist
-  if (req.query.month && req.query.year) {
-    return getMonthlyIncome(req, res, next);
-  }
-  return getIncomes(req, res, next);
-});
-
+router.get("/", protect, generalLimiter, getIncomes);
 router.put("/:id", protect, generalLimiter, updateIncome);
 router.delete("/:id", protect, generalLimiter, deleteIncome);
 
